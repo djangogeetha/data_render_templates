4b7b7f4ae2a4ec22c4da1812e031c22f0d5033be
djangogeetha <geethaputtur369@gmail.com>
2023-07-27 12:18:18 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -3,6 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 
 def  data_render(request):
-    d={'name':'GEETHA'}
+    d={'name':'GEETHA','age':12}
     return render(request,'data_render.html',context=d)
 
